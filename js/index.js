02c717006d6bc0cba9aeb314c36175e3b3789d8d
sweetsoph <sophia.verardods@gmail.com>
2023-05-02 11:39:16 -0300

    reorganizing paragraphs

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 function changeCss () {
     var header = document.querySelector("header");
-    this.scrollY > 150 ? header.style.backgroundColor = "#161616" : header.style.backgroundColor = "transparent";
+    var h3 = document.getElementsByClassName(".h3");
+    this.scrollY > 150 ? (header.style.backgroundColor = "#161616", h3.style.color = "white") : (header.style.backgroundColor = "transparent", h3.style.color = "#");
   }
   window.addEventListener("scroll", changeCss , false);
